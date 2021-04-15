f7b924e9994f19fd36e578f5210443a261bf4862
Mateus Guimarães Alves <guimaraesalves45@gmail.com>
2021-04-15 09:47:48 -0300

    Update exe_12-04-21.py

--- Python/Python inicial/CadernoExercicios02/exe_12-04-21.py
+++ Python/Python inicial/CadernoExercicios02/exe_12-04-21.py
@@ -1,16 +1,17 @@
-def namelist(names):
-    res = ''
-    if(len(names) == 1):
-        return  names[0]['name']
-    elif(len(names) == 2):
-        res = res + names[0]['name'] + " & " + names[1]['name']
-    elif(len(names) > 2):
-        for i in range(0, len(names) - 1):
-            res = res + names[i]['name'] + ", "
-        res = res[:2] + " & " + names[len(names) - 1]['name']
-    return res
+from math import pi
+diametro = float(input("Diâmetro(mm): "))
+altura = float(input("Altura(mm): "))
+
+def calculoVolumeCopo(diametro, altura):
+    raio = diametro/2
+    volume_cm = raio**2 * pi /100
+
+    volume_mililitros = volume_cm / 1000
 
 
-for contA in range(1, 10):
-    for contB in range((contA + 1), 11):
-        if vet[contA] > vet[contB]:
+
+    return print(round(volume_cm, 2), "mm³. ", round(volume_mililitros, 2), 'Litros' )
+
+
+calculoVolumeCopo(diametro, altura)
+
